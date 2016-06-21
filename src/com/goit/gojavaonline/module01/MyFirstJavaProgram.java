e801dd074aadbeb12f864a220456de2691f4369b
danil <ostapchenko.danil@gmail.com>
2016-06-22 02:34:05 +0300

    initial commit

---
+++ src/com/goit/gojavaonline/module01/MyFirstJavaProgram.java
@@ -0,0 +1,7 @@
+package com.goit.gojavaonline.module01;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi! I\'m your first Java program! Stay Tuned!");
+    }
+}

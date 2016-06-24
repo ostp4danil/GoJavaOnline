6334290ad95229811277478bc8405bea13768c78
danil <ostapchenko.danil@gmail.com>
2016-06-24 11:54:42 +0300

    fixed flowers

--- src/com/goit/gojavaonline/module03/first/Bouquet.java
+++ src/com/goit/gojavaonline/module03/first/Bouquet.java
@@ -3,7 +3,8 @@ package com.goit.gojavaonline.module03.first;
 public class Bouquet {
 
     {
-        Flower firstRose = new Rose();
+        Flower first = new Rose();
+        RoseBush second = new RoseBush();
     }
 
 

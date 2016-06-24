a9f5893f84a4bb66bdee14eda4991efabd2657a6
danil <ostapchenko.danil@gmail.com>
2016-06-24 10:38:22 +0300

    fixed flowers

--- src/com/goit/gojavaonline/module04/first/Runner.java
+++ src/com/goit/gojavaonline/module04/first/Runner.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * Created by Danil-MAC on 22.06.16.
  */
 public abstract class Runner {
+
     public static void main(String[] args) {
 
         Area[] figures = new Area [5];

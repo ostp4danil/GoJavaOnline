9fa720e05506a5fd8c275820d29f22d615545493
danil <ostapchenko.danil@gmail.com>
2016-07-25 09:03:41 +0300

    10

--- src/com/goit/gojavaonline/module10/Runner.java
+++ src/com/goit/gojavaonline/module10/Runner.java
@@ -7,30 +7,22 @@ import java.io.IOException;
 
 public class Runner {
     public static void main(String[] args) {
-        CaesarsСipher cipher = new CaesarsСipher("qwerty", 4);
+        CaesarsСipher cipher = new CaesarsСipher("qwerty", 2);
         StreamOerations streamOerations = new StreamOerations("Test");
         File inputFile = new File("example.txt");
         File encodedFile = new File("encoded.txt");
         File decodedFile = new File("decoded.txt");
 
         try {
-            String string = streamOerations.readFromFile(inputFile);
-            String encoded = cipher.encode(string);
+            String input1 = streamOerations.readFromFile(inputFile);
+            String encoded = cipher.encode(input1);
+            System.out.println(encoded);
             streamOerations.writeToFile(encoded, encodedFile);
-            System.out.println(inputFile.getName() + "\'s data was successfully written to "
-                    + encodedFile.getName());
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-
-        try {
-            encodedFile.delete();
-            String string = streamOerations.readFromFile(encodedFile);
-            String decoded = cipher.encode(string);
+            System.out.println(inputFile.getName() + " was written into the " + encodedFile.getName());
+            String input2 = streamOerations.readFromFile(encodedFile);
+            String decoded = cipher.decode(input2);
             streamOerations.writeToFile(decoded, decodedFile);
-
-            System.out.println(encodedFile.getName() + "\'s data was successfully written to "
-                    + decodedFile.getName());
+            System.out.println(encodedFile.getName() + " was written into the " + decodedFile.getName());
         } catch (IOException e) {
             System.err.println(e);
         }
